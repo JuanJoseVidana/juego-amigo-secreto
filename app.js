bc00f92e3845f9aea6448f17a6d3db2cd85fa026
Juan Vidana <jjvidaher@hotmail.com>
2025-03-10 22:03:49 -0600

    Agrear CSS y HTML para partir de una base

---
+++ app.js
@@ -0,0 +1,21 @@
+/*
+Título: Reto de Lógica de Programación
+Autor: Juan Vidaña
+Fecha de inicio: 10 de marzo de 2025
+
+        Descripción:
+Este script implementa una aplicación donde los usuarios pueden agregar nombres de amigos a una lista 
+y seleccionar aleatoriamente un 'Amigo Secreto'. El programa incluye validación de entrada, 
+visualización de la lista y una funcionalidad de sorteo aleatorio.
+
+        Características:
+- Agregar nombres a la lista
+- Validar entradas vacías
+- Mostrar la lista actualizada en tiempo real
+- Seleccionar y mostrar un nombre aleatoriamente como el Amigo Secreto
+
+Uso: Simplemente vincula este script a un archivo HTML, y la aplicación manejará todas las interacciones.
+
+Última actualización: 10/03/2025
+*/
+
